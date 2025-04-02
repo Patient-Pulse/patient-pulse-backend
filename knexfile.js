fcfac08c20f8a13482d11c2eca690bc790aaa204
Mahesh Morem <maheshmorem1787@gmail.com>
2025-04-02 16:55:45 +0530

    using ENV for DB config

--- knexfile.js
+++ knexfile.js
@@ -2,15 +2,15 @@ export default {
   development: {
     client: 'mysql2',
     connection: {
-      host: '127.0.0.1',
-      user: 'root',
-      password: 'root@123',
-      database: 'pulspoint',
+      host: process.env.HOST, //'127.0.0.1',
+      user: process.env.USER, //root
+      password: process.env.PASSWORD, //root@123
+      database: process.env.DATABASE, //'pulspoint'
       charset: 'utf8mb4',
     },
     pool: {
       min: 2,
-      max: 10, // Increase this value based on your needs
+      max: 10,
     },
     migrations: {
       directory: './migrations',
