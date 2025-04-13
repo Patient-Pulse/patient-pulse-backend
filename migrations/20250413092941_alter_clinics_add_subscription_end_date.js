f2d7d9ac19d439d6b0ca7ffacef89dedee563356
Mahesh Morem <maheshmorem1787@gmail.com>
2025-04-13 16:25:15 +0530

    subscription expiry feature

---
+++ migrations/20250413092941_alter_clinics_add_subscription_end_date.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = function (knex) {
+  return knex.schema.alterTable("clinics", (table) => {
+    table.timestamp("subscription_end_date").nullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = function (knex) {
+  return knex.schema.alterTable("clinics", (table) => {
+    table.dropColumn("subscription_end_date");
+  });
+};
